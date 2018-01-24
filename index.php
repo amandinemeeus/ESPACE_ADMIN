322f1bd365df10dd71ee1331f4ee089a1f919c1e
amandinemeeus <amandine.meeus@alumni.ulb.be>
2018-01-24 11:19:11 +0100

    nav

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Burger Code</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <div class="container site">
+            <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger Code <span class="glyphicon glyphicon-cutlery"></span></h1>
+            <nav>
+                <ul>
+                    <li role="presentation" class="active"><a href="#1" data-toggle="tab">Menus</a></li>
+                    <li role="presentation"><a href="#2" data-toggle="tab">Burgers</a></li>
+                    <li role="presentation"><a href="#3" data-toggle="tab">Snacks</a></li>
+                    <li role="presentation"><a href="#4" data-toggle="tab">Salades</a></li>
+                    <li role="presentation"><a href="#5" data-toggle="tab">Boissons</a></li>
+                    <li role="presentation"><a href="#6" data-toggle="tab">Desserts</a></li>
+                </ul>
+            </nav>
+        </div>
+    </body>
